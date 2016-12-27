8c1a3a67e6b2e696ad7e842ec5d9330f75d54b93
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2016-12-28 03:20:53 +0530

    Corrected entry point to adapt to the new file structure.

--- main.lisp
+++ main.lisp
@@ -1,7 +1,13 @@
 ;; Entry point that runs the server.
 
-;; Load the server script
+;; Load the header parser
+(load "src/parse-header.lisp")
+
+;; Load the main server script
 (load "src/server.lisp")
+
+;; Load the request handler to be passed to serve function
+(load "src/request-handler.lisp")
 
 ;; Run the server
 (serve #'hello-request-handler)
